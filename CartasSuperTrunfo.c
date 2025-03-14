bbf41a2ea5880e1b9d6372f04ebce2e8f973f733
Carlos Daniel <megadestroier@hotmail.com>
2025-03-14 23:29:11 +0000

    Desafio3 adicionando comparação para saber qual cara venceu em cada aspecto

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,7 +10,7 @@ int main() {
 
     //carta numero 1
 int numeroDePontosTuristicosUm; // pontos turísticos carta 1
-int populacaoUm;  // População carta 1
+unsigned long int populacaoUm;  // População carta 1
 char codigoDaCartaUm[5]; // código da carta 1
 char nomeDaCidadeUm[15]; // nome da cidade carta 1
 char estadoUm[2];  // sigla do estado carta 1
@@ -20,7 +20,7 @@ float pibUm;  // PIB da carta 1
 
    //carta numero 2
 int numeroDePontosTuristicosDois; // pontos turísticos carta 2
-int populacaoDois;  // População carta 2
+unsigned long int populacaoDois;  // População carta 2
 char codigoDaCartaDois[5]; // código da carta 2
 char nomeDaCidadeDois[20]; // nome da cidade carta 2
 char estadoDois[2];  // sigla do estado carta 2
@@ -87,6 +87,11 @@ float PibPerCapitaDois = (pibDois / populacaoDois); //carta 2
 float DensidadePopulacionalUm = (populacaoUm / areaEmKmUm); //carta 1
 float DensidadePopulacionalDois = ( populacaoDois / areaEmKmDois); //carta 2
 
+// calcular super poder
+float superPoder1 = populacaoUm + areaEmKmUm + pibUm + numeroDePontosTuristicosUm + PibPerCapitaUm + DensidadePopulacionalUm; // carta 1
+float superPoder2 = populacaoDois + areaEmKmDois + pibDois + numeroDePontosTuristicosDois + PibPerCapitaDois + DensidadePopulacionalDois; // carta 2
+
+
 
 //exibindo dados da carta 1
 printf("\nCódigo da carta N°1: %s\n", codigoDaCartaUm); // exibe código 
@@ -96,8 +101,9 @@ printf("Área: %.2f Km² \n", areaEmKmUm); // exibe a area em km²
 printf("População total: %d\n", populacaoUm); // exibe a população
 printf("Número de pontos turísticos: %d\n", numeroDePontosTuristicosUm); // exibe o numero de pontos tuisticos
 printf("PIB: %.2f Bilhões de reais \n", pibUm); // exibe o pib 
-printf("Densidade Populacional: %.2f hab/km² \n",DensidadePopulacionalUm); //exibe a densidade populacional de habitantes por Km²
+printf("Densidade Populacional: %.2f hab/km²\n",DensidadePopulacionalUm); //exibe a densidade populacional de habitantes por Km²
 printf("PIB per Capita: %.2f reais \n",PibPerCapitaUm ); //exibe o pib per capita
+printf("O Super poder é: %.2f\n",superPoder1); // exibe o super poder 
 
 
 
@@ -110,8 +116,36 @@ printf("População total: %d\n", populacaoDois); // exibe a população
 printf("Número de pontos turísticos: %d\n", numeroDePontosTuristicosDois); // exibe o numero de pontos tuisticos 
 printf("PIB: %.2f Bilhões de reais \n", pibDois); // exibe o pib 
 printf("Densidade Populacional: %.2f hab/km² \n",DensidadePopulacionalDois); //exibe a densidade populacional de habitantes por Km²
-printf("PIB per Capita: %.2f reais \n",PibPerCapitaDois ); //exibe o pib per capita 
+printf("PIB per Capita: %.2f reais\n",PibPerCapitaDois ); //exibe o pib per capita 
+printf("O Super poder é: %.2f\n",superPoder2 ); // exibe o super poder 
 
+
+//comparação das cartas
+printf("\n****Comparação de Cartas: Onde 1 equivale a vitoria (True), 0 equivale a perda(False)****\n");
+
+printf("População: Carta 1 venceu (%d)\n",
+       populacaoUm > populacaoDois  
+);
+printf("Área: Carta 1 venceu (%d)\n",
+       areaEmKmUm > areaEmKmDois        
+);
+printf("PIB: Carta 1 venceu (%d)\n",
+       pibUm > pibDois
+);
+printf("Pontos Turísticos: Carta 1 venceu (%d)\n",
+       numeroDePontosTuristicosUm > numeroDePontosTuristicosDois    
+);
+printf("Densidade Populacional: Carta 2 venceu (%d)\n", 
+       DensidadePopulacionalUm < DensidadePopulacionalDois
+);
+printf("PIB per Capita: Carta 1 venceu (%d)\n",
+       PibPerCapitaUm > PibPerCapitaDois
+);
+printf("Super Poder: Carta 1 venceu (%d)\n",
+       superPoder1 > superPoder2
+);
 return 0;
 
 }
+
+
